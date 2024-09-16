c7364375aff6ff3c65edaba0b2b2cfa2e00e4b00
hgaba1611 <harshitgaba161111@gmail.com>
2024-09-16 15:02:17 +0530

    Update StringMethods.py

--- Python/StringMethods.py
+++ Python/StringMethods.py
@@ -1,1 +1,17 @@
-print("hello world!!!!")  
+#Strings are immutable
+a = "Harshit!!!!" 
+print(len(a))
+print(a.upper()) # Creats a new string and print it in UPPERCASE
+print(a.lower()) #Creats a new string and prints it in lowercase
+print(a.rstrip()) #Creats a new string and strips the trailing characters
+print(a.replace("Harry", "John")) #Creats a new string and replaces the occurence of the string with another string
+print(a.split("  ")) #Creats a new string and splits the given string at the specified instance
+print(a.count("Harshit")) #Tells the no of times the given string is occuring
+
+blogHeading = "introduction tO Chess"
+print(blogHeading.capitalize()) #Creats a new string and capitalizes the first letter and lowercases all the other letters
+ str1 = "Welcome to the console!!!!"
+print(str1.center(50)) #It will create a new string and add spaces to center the string
+print(str1.endswith("!!!!"))
+
+
