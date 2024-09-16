0efe47a82a45b85c7fc31fe55ee6ede5affc704d
hgaba1611 <harshitgaba161111@gmail.com>
2024-09-16 18:04:31 +0530

    Update StringMethods.py

--- Python/StringMethods.py
+++ Python/StringMethods.py
@@ -10,11 +10,21 @@ print(a.count("Harshit")) #Tells the no of times the given string is occuring
 
 blogHeading = "introduction tO Chess"
 print(blogHeading.capitalize()) #Creats a new string and capitalizes the first letter and lowercases all the other letters
-str1 = "Welcome to the console!!!!"
+str1 = "!!!!Welcome to the console!!!!"
 print(str1.center(50)) #It will create a new string and add spaces to center the string
 print(str1.endswith("!!!!"))#Answers in true or false if the variable ends with a given string
+print(str1.startswith("!!!!"))#Answers in true only if the variable starts with a given string
 
 str2 = "He's name is Dan. He is an honest man."
 print(str2.find("is")) # prints the index of the occurence of the given string
-print(str2.index("ishh")) #prints an error if the given string is not present
+#print(str2.index("ishh")) #prints an error if the given string is not present
 print(str2.isalnum()) #It returns true only if the entire string consistsof A-Z,a-z and 0-9.Otherwise it will return false
+print(str2.isalpha()) #It returns true only if the entire string consistsof A-Z,a-z.Otherwise it will return false
+print(str2.islower()) #It returns true only if the entire string is in lowercase
+print(str2.isupper()) #It returns true only if the entire string is in UPPERCASE
+print(str2.isprintable()) #It returns true only if the entire string is printable
+print(str2.isspace()) #It returns true only if the entire string is in white space using spaces ot tabs
+print(str2.istitle()) #It returns true only if the first letter of each word in string is capitalized
+print(str2.istitle()) #It returns true only if the first letter of each word in string is capitalized
+print(str2.swapcase()) #It comverts uppercase to lowercase and lowercase to uppercase.
+print(str2.title()) #It converts the whole string into title.
